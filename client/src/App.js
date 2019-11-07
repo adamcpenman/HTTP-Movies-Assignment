f7fc54186e0ac879c5a037b8a52361ca7c6e247e
Your Name <you@example.com>
2019-11-07 17:21:06 -0600

    have an edit function and api call

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import MovieUpdate from "./Movies/UpdateMovie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -15,11 +16,13 @@ const App = () => {
     <>
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList} />
+      <Route exact path="/update-movie/:id" component={MovieUpdate} />
       <Route
         path="/movies/:id"
         render={props => {
           return <Movie {...props} addToSavedList={addToSavedList} />;
         }}
+        
       />
     </>
   );
